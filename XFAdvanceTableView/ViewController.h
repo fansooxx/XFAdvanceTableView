19c34a3246316ad9986babf66a800567126bd650
fansooxx <15901795306@163.com>
2018-02-25 12:15:36 +0800

    Initial Commit

---
+++ XFAdvanceTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XFAdvanceTableView
+//
+//  Created by 谢帆 on 2018/2/25.
+//  Copyright © 2018年 谢帆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XFAdvanceTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XFAdvanceTableView
+//
+//  Created by 谢帆 on 2018/2/25.
+//  Copyright © 2018年 谢帆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
